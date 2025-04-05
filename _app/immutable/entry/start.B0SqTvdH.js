0821950eb12a58c909b3b78afd36ad75117d97a7
Jason Desrosiers <jdesrosi@gmail.com>
2025-04-05 15:35:22 -0700

    Update to 1.12.0

---
+++ _app/immutable/entry/start.B0SqTvdH.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/w7m1DCEw.js";export{o as load_css,r as start};
--- _app/immutable/nodes/0.Esi_AtRL.js
+++
@@ -1,1 +0,0 @@
-import{c as n,a}from"../chunks/DwFe8hMG.js";import{f as p}from"../chunks/BQfsNb37.js";import{s}from"../chunks/CMhveyFG.js";const i=!0,l=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,l as universal};
