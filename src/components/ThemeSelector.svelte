8285f6f4659977cee6eb3742074df871603f0506
Aditya Yadav <aaditya1805y@gmail.com>
2026-03-07 22:37:25 +0530

    fix: improve theme dropdown visibility in light mode

--- src/components/ThemeSelector.svelte
+++ src/components/ThemeSelector.svelte
@@ -98,6 +98,7 @@
     --item-height: 1.5em;
 
     --font-size: 1em;
+    color: var(--text-color);
 
     --chevron-width: 100%;
     --chevron-icon-width: 1em;
@@ -111,6 +112,11 @@
     --item-hover-bg: var(--line-focus-background-color);
     --selected-item-bg: var(--line-focus-background-color);
     --item-is-active-bg: var(--line-focus-background-color);
+
+    --item-color: var(--text-color);
+    --item-hover-color: var(--text-color);
+    --item-is-active-color: var(--text-color);
+    --item-active-color: var(--text-color);
 
     --border: thin solid var(--text-color);
     --border-focused: thin solid var(--text-color);
