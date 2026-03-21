4bfa11d257d380ba592a0c49ef943f94b14c9d10
Jason Desrosiers <jdesrosi@gmail.com>
2026-03-21 11:16:06 -0700

    Refactor the share button to its own component

---
+++ src/components/LinkIcon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let { size = "1.25rem" } = $props();
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  fill="none"
+>
+  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" fill="none"></path>
+  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" fill="none"></path>
+</svg>
